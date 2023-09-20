826803e06581d4fbd989582d15a3b335f96c342a
sarakhaled95 <architectsarakhaled@outlook.com>
2023-09-20 09:21:41 -0700

    main.c

--- main.c
+++ main.c
@@ -19,4 +19,65 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
+/**
+ * create_node - Creates a node
+ * @n: Number to go inside of node
+ * Return: Upon sucess a pointer to the node, NULL if fail
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
 
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		error(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack
+ * Return: void
+ */
+void free_nodes(void)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+/**
+ * add_to_queue - adds a node to the queue
+ * @new_node: Pointer to the new node
+ * @ln: line number
+ * Return: void
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = temp;
+
+}
