091d0255eddfdf38dcf9f4fbeb2be6ea2198e01f
sarakhaled95 <architectsarakhaled@outlook.com>
2023-09-21 05:44:40 -0700

    forgot to make directory

---
+++ monty_files/file_op.c
@@ -0,0 +1,148 @@
+#include "monty.h"
+
+/**
+ * open_file - opens a file
+ * @file_name: file name
+ * Return: Always zero
+ */
+void open_file(char *file_name)
+{
+	FILE *fp;
+
+	fp = fopen(file_name, "r");
+
+	if (file_name == NULL || fp == NULL)
+		error(2, file_name);
+
+	read_file(fp);
+	fclose(fp);
+}
+
+/**
+ * read_file - reads a file
+ * @fp: pointer to file descreptor
+ * Return: void
+ */
+void read_file(FILE *fp)
+{
+	int L_num, format = 0;
+	char *buffer = NULL;
+	size_t len = 0;
+
+	for (L_num = 1; getline(&buffer, &len, fp) != -1; L_num++)
+	{
+		format = parse(buffer, L_num, format);
+	}
+	free(buffer);
+}
+
+/**
+ * parse - sparates each line to token to know which func to call
+ * @buffer: line from file
+ * @line_number: line number
+ * @format: storage format. 0 will be entered as stack
+ * 1 will be entered as queue
+ * Return: 0 if opcode stack 1 if queue
+ */
+int parse(char *buffer, int line_number, int format)
+{
+	char *opcode, *val;
+	const char *delim = "\n";
+
+	if (buffer == NULL)
+		error(4);
+
+	opcode = strtok(buffer, delim);
+	if (opcode == NULL)
+		return (format);
+	val = strtok(NULL, delim);
+
+	if (strcmp(opcode, "stack") == 0)
+		return (0);
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+
+	find_function(opcode, val, line_number, format);
+	return (format);
+}
+
+/**
+ * find_function - find the appropriate function for opcode
+ * @opcode: opcode
+ * @val: arg of opcode
+ * @L_num: line number
+ * @format: storage format. 0 will be entered as stack
+ * 1 will be entered as queue
+ * Return: void
+ */
+void find_function(char *opcode, char *val, int L_num, int format)
+{
+	int j;
+	int flag;
+
+	instruction_t list_functions[] = {
+		{"push", push},
+		{"pall", print_all},
+		{"pint", print_top},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add_node},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+
+	if (opcode[0] == '#')
+		return;
+
+	for (flag = 1, j = 0; list_functions[j].opcode != NULL; j++)
+	{
+		if (strcmp(opcode, list_functions[j].opcode) == 0)
+		{
+			call_func(list_functions[j].f, opcode, val, L_num, format);
+			flag = 0;
+		}
+	}
+	if (flag == 1)
+		error(3, L_num, opcode);
+}
+/**
+ * call_func - calls the function
+ * @func: pointer to funtion
+ * @op: string representing opcode
+ * @val: string rep a num value
+ * @L_num: line number
+ * @format: storage format. 0 will be entered as stack
+ * 1 will be entered as queue
+ * Return: void
+ */
+void call_func(op_func func, char *op, char *val, int L_num, int format)
+
+{
+	stack_t *node;
+	int flag;
+	int k;
+
+	flag = 1;
+	if (strcmp(op, "push") == 0)
+	{
+		if (val != NULL && val[0] == '-')
+		{
+			val += 1;
+			flag = -1;
+		}
+		if (val == NULL)
+			error(5, L_num);
+		for (k = 0; val[k] != '\0'; k++)
+		{
+			if (isdigit(val[k] == 0))
+				error(5, L_num);
+		}
+		node = create_node(atoi(val) * flag);
+		if (format == 0)
+			func(&node, L_num);
+		if (format == 1)
+			add_to_queue(&node, L_num);
+	}
+	else
+		func(&head, L_num);
+}
---
+++ monty_files/monty.h
@@ -0,0 +1,70 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern stack_t *head;
+typedef void (*op_func)(stack_t **stack, unsigned int);
+
+/* File I/O file_op.c */
+void open_file(char *file_name);
+void read_file(FILE *);
+int parse(char *buffer, int L_num, int format);
+void call_func(op_func func, char *op, char *val, int L_num, int format);
+void find_function(char *, char *, int, int);
+
+/* error handling in error.c */
+void error(int error_code, ...);
+void more_error(int error_code, ...);
+
+/*main.c*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln);
+
+/* mandatory.c */
+void push(stack_t **new_node, __attribute__((unused))unsigned int ln);
+void print_all(stack_t **stack, unsigned int L_num);
+void print_top(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int L_num);
+void swap(stack_t **stack, unsigned int L_num);
+
+void add_node(stack_t **stack, unsigned int L_num);
+void nop(stack_t **stack, unsigned int L_num);
+#endif
